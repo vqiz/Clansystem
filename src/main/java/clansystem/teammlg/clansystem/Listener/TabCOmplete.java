03277ce32676611672d180e693a221f65bceb3a6
BuildTools <unconfigured@null.spigotmc.org>
2023-05-16 14:33:05 +0200

    update

--- src/main/java/clansystem/teammlg/clansystem/Listener/TabCOmplete.java
+++
@@ -1,9 +0,0 @@
-package clansystem.teammlg.clansystem.Listener;
-
-import net.md_5.bungee.api.event.TabCompleteEvent;
-import net.md_5.bungee.api.plugin.Listener;
-import net.md_5.bungee.event.EventHandler;
-
-public class TabCOmplete implements Listener {
-
-}
