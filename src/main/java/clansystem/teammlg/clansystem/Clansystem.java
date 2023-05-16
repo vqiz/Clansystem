3f01e9d990f96c0e55bc1bc0f8679fdfbea3779e
BuildTools <unconfigured@null.spigotmc.org>
2023-05-16 16:06:21 +0200

    update

--- src/main/java/clansystem/teammlg/clansystem/Clansystem.java
+++ src/main/java/clansystem/teammlg/clansystem/Clansystem.java
@@ -28,18 +28,18 @@ import java.io.IOException;
 
 public final class Clansystem extends Plugin {
   public Table user = new Table("systembanh214", "UUID TEXT, NAME TEXT, IP TEXT, SERVER TEXT");
-    public Gson gson = new GsonBuilder().create();
-     public static Clansystem instance;
-     public Table clanmanager = new Table("Cmanager","NAME TEXT,TAG TEXT, DATA TEXT");
-    public Table clanusermanager = new Table("Cmusermanager","UUID TEXT,NAME TEXT");
-     public static String prefix;
-    public static Clansystem getInstance(){
+  public Gson gson = new GsonBuilder().create();
+  public static Clansystem instance;
+  public Table clanmanager = new Table("Cmanager","NAME TEXT,TAG TEXT, DATA TEXT");
+  public Table clanusermanager = new Table("Cmusermanager","UUID TEXT,NAME TEXT");
+  public static String prefix;
+  public static Clansystem getInstance(){
         return instance;
     }
-    public static DatabaseManager db;
-    Configuration config = null;
-    @Override
-    public void onEnable() {
+  public static DatabaseManager db;
+  Configuration config = null;
+  @Override
+  public void onEnable() {
         instance = this;
         config = makeConfig();
         db = new DatabaseManager(config.getString("mysql.host"), config.getString("mysql.user"), config.getString("mysql.database"), config.getString("mysql.password"));
@@ -58,7 +58,7 @@ public final class Clansystem extends Plugin {
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new Clan());
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new Clanadmin());
     }
-    public Configuration makeConfig() {
+  public Configuration makeConfig() {
         /*  75 */     if (!getDataFolder().exists()) {
             /*  76 */       getLogger().info("Created config folder: " + getDataFolder().mkdir());
             /*     */     }
@@ -96,14 +96,14 @@ public final class Clansystem extends Plugin {
         /* 111 */     return config;
         /*     */   }
 
-    @Override
-    public void onDisable() {
+  @Override
+  public void onDisable() {
         // Plugin shutdown logic
     }
-    /*     */   private String addDefault(Configuration conf, String path, String test1) {
-        /* 117 */     if (!conf.contains(path)) {
-            /* 118 */       conf.set(path, test1);
-            /*     */     }
-        /* 120 */     return conf.getString(path);
-        /*     */   }
+  private String addDefault(Configuration conf, String path, String test1) {
+      if (!conf.contains(path)) {
+          conf.set(path, test1);
+      }
+      return conf.getString(path);
+  }
 }
